10eeb314bf0e2bef92084a1e9b3e1c0a32b49793
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-03-12 09:56:46 -0300

    src

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   entry: ['src/http/server.ts'],
   outDir: 'dist',
   format: ['cjs'],
-  target: 'node18', 
-  clean: true, 
-  dts: false, 
+  target: 'node18',
+  clean: true,
+  dts: false,
   minify: false,
   sourcemap: true,
 });
